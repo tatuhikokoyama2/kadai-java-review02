28f0d18edb92edcd010ddeaf9857d55b653a60c3
TATUHIKO <TATUHIKO@192.168.1.9>
2023-01-25 23:00:58 +0900

    lesson4課題作成

--- src/kadai/Review02.java
+++ src/kadai/Review02.java
@@ -4,7 +4,22 @@ public class Review02 {
 
     public static void main(String[] args) {
         for(int i= 1; i < 101; i++) {
-            System.out.println(i);
+            //3で割り切れるかつ5で割り切れる
+            if((i % 3 == 0) && (i % 5 == 0)) {
+                System.out.println("FizzBuzz");
+            }
+            //数が3で割り切れる
+            else if(i % 3 == 0) {
+                System.out.println("Fizz");
+            }
+            //数が5で割り切れる
+            else if(i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            //そのほかの場合
+            else {
+                System.out.println(i);
+            }
         }
 
     }
